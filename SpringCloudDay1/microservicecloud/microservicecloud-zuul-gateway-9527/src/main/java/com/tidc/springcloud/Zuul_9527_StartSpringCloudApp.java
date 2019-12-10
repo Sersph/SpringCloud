529625662f1c95b294deca09ad79e5ebd87fcc46
Sersph <1033278494@qq.com>
2019-12-10 17:03:20 +0800

    5、SpringCloud整合Zuul（路由网关）

---
+++ SpringCloudDay1/microservicecloud/microservicecloud-zuul-gateway-9527/src/main/java/com/tidc/springcloud/Zuul_9527_StartSpringCloudApp.java
@@ -0,0 +1,18 @@
+package com.tidc.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @Author Kay
+ * @Date 2019/12/10 16:37
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class Zuul_9527_StartSpringCloudApp {
+    public static void main(String[] args) {
+        SpringApplication.run(Zuul_9527_StartSpringCloudApp.class, args);
+    }
+}
